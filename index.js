3ff28d52dedfd5c74168fda9a6250ebe249cf8da
tom.pp.lpt <120770345+pp13coolpowder@users.noreply.github.com>
2023-11-02 13:48:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const port = 3000;
 const app = express();
 app.use(express.json());
 app.use(helmet());
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 app.listen(port, console.log(`Server Running on ${port}`));
